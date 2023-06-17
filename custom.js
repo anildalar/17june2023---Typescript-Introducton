0a5e11d922433cd869ab6d0128454d7a4f67d222
anildalar <dollorinfotech@gmail.com>
2023-06-17 09:14:37 +0530

    new

---
+++ custom.js
@@ -0,0 +1,5 @@
+var name1 = "ANIL"; //decleration and initialization
+console.log(name1);
+var age; // decleration
+age = 30;
+console.log(age);
---
+++ custom.ts
@@ -0,0 +1,11 @@
+
+var name1:string="ANIL"; //decleration and initialization
+
+console.log(name1);
+
+var age:number; // decleration
+
+
+age=30
+
+console.log(age);
